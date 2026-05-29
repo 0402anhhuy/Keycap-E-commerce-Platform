2b0643d0a841c704e627b87f56accc7c2dfbe7fc
TranAnhHuy <anhhuyrubic@gmail.com>
2026-05-30 00:15:40 +0700

    feat: implement complete frontend shopping cart functionality and user interface structure

---
+++ ReactJS_FE/src/App.jsx
@@ -0,0 +1,47 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/user/Profile";
+import HomePage from "./pages/user/HomePage";
+import ProductPage from "./pages/user/ProductPage";
+import ProductDetail from "./pages/user/ProductDetail";
+import CartPage from "./pages/user/CartPage";
+import CheckoutPage from "./pages/user/CheckoutPage";
+import OrdersPage from "./pages/user/OrdersPage";
+import OrderDetailPage from "./pages/user/OrderDetailPage";
+import WishlistPage from "./pages/user/WishlistPage";
+import VendorOnboarding from "./pages/vendor/VendorOnboarding";
+import VendorDashboard from "./pages/vendor/VendorDashboard";
+import ManagerDashboard from "./pages/manager/ManagerDashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import { CartProvider } from "./context/CartContext";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <CartProvider>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+
+                    <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+                    <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+                    <Route path="/products" element={<ProtectedRoute><ProductPage /></ProtectedRoute>} />
+                    <Route path="/product/:id" element={<ProtectedRoute><ProductDetail /></ProtectedRoute>} />
+                    <Route path="/cart" element={<ProtectedRoute><CartPage /></ProtectedRoute>} />
+                    <Route path="/checkout" element={<ProtectedRoute><CheckoutPage /></ProtectedRoute>} />
+                    <Route path="/orders" element={<ProtectedRoute><OrdersPage /></ProtectedRoute>} />
+                    <Route path="/orders/:id" element={<ProtectedRoute><OrderDetailPage /></ProtectedRoute>} />
+                    <Route path="/wishlist" element={<ProtectedRoute><WishlistPage /></ProtectedRoute>} />
+                    <Route path="/vendor/setup" element={<ProtectedRoute><VendorOnboarding /></ProtectedRoute>} />
+                    <Route path="/vendor/dashboard" element={<ProtectedRoute><VendorDashboard /></ProtectedRoute>} />
+                    <Route path="/manager/dashboard" element={<ProtectedRoute><ManagerDashboard /></ProtectedRoute>} />
+                </Routes>
+            </CartProvider>
+        </BrowserRouter>
+    );
+}
+
+export default App;
