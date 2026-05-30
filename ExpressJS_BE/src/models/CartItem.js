09b0ee3fdd47da74f042aded4e60d11020b7a7b2
TranAnhHuy <anhhuyrubic@gmail.com>
2026-05-30 16:25:48 +0700

    feat: implement base infrastructure including user authentication, data models, and storefront product browsing components

--- ExpressJS_BE/src/models/CartItem.js
+++ ExpressJS_BE/src/models/CartItem.js
@@ -1,12 +1,45 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
 
-const CartItem = sequelize.define('CartItem', {
-    id: { type: DataTypes.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
-    userId: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
-    productId: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
-    quantity: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 1 },
-    color: { type: DataTypes.STRING(50), allowNull: true }
-}, { tableName: 'cart_items', timestamps: true });
+const CartItem = sequelize.define(
+    "CartItem",
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+
+        userId: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+        },
+
+        productId: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+        },
+
+        quantity: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            defaultValue: 1,
+            validate: {
+                min: 1,
+            },
+        },
+    },
+    {
+        tableName: "cart_items",
+        timestamps: true,
+
+        indexes: [
+            {
+                unique: true,
+                fields: ["userId", "productId"],
+            },
+        ],
+    },
+);
 
 module.exports = CartItem;
