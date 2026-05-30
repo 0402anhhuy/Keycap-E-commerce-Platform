c04191c997c34b6c34196ca02adf4e872fc930e9
TranAnhHuy <anhhuyrubic@gmail.com>
2026-05-30 23:46:50 +0700

    feat: implement full-stack authentication, routing, order management, and core services for the e-commerce shop

--- ExpressJS_BE/src/controllers/reviewController.js
+++ ExpressJS_BE/src/controllers/reviewController.js
@@ -9,24 +9,6 @@ const createProductReview = async (req, res) => {
     }
 };
 
-const createShopReview = async (req, res) => {
-    try {
-        const review = await reviewService.createShopReview(req.user.id, req.body);
-        return res.status(201).json(review);
-    } catch (err) {
-        return res.status(err.status || 500).json({ message: err.message });
-    }
-};
-
-const createOrderReview = async (req, res) => {
-    try {
-        const review = await reviewService.createOrderReview(req.user.id, req.body);
-        return res.status(201).json(review);
-    } catch (err) {
-        return res.status(err.status || 500).json({ message: err.message });
-    }
-};
-
 const getProductReviews = async (req, res) => {
     try {
         const result = await reviewService.getProductReviews(req.params.productId, req.query);
@@ -36,31 +18,13 @@ const getProductReviews = async (req, res) => {
     }
 };
 
-const getShopReviews = async (req, res) => {
+const getManagerReviews = async (req, res) => {
     try {
-        const result = await reviewService.getShopProductReviews(req.shop.id, req.query);
+        const result = await reviewService.getManagerReviews(req.query);
         return res.json(result);
     } catch (err) {
         return res.status(err.status || 500).json({ message: err.message });
     }
 };
 
-const vendorReplyReview = async (req, res) => {
-    try {
-        const review = await reviewService.vendorReplyReview(req.user.id, req.params.id, req.body.reply);
-        return res.json(review);
-    } catch (err) {
-        return res.status(err.status || 500).json({ message: err.message });
-    }
-};
-
-const setReviewVisibility = async (req, res) => {
-    try {
-        const review = await reviewService.setReviewVisibility(req.params.type, req.params.id, req.body.isHidden);
-        return res.json(review);
-    } catch (err) {
-        return res.status(err.status || 500).json({ message: err.message });
-    }
-};
-
-module.exports = { createProductReview, createShopReview, createOrderReview, getProductReviews, getShopReviews, vendorReplyReview, setReviewVisibility };
+module.exports = { createProductReview, getProductReviews, getManagerReviews };
