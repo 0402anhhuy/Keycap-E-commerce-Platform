62b91d8e03e994b272c2ea25591cbebef2cc0b8e
TranAnhHuy <anhhuyrubic@gmail.com>
2026-06-05 01:19:12 +0700

    feat: initialize Express backend server, authentication services, product management logic, and database schemas

--- ExpressJS_BE/src/seeders/collectionSeeder.js
+++ ExpressJS_BE/src/seeders/collectionSeeder.js
@@ -4,39 +4,57 @@ const Product = require("../models/Product");
 const COLLECTION_DATA = [
     {
         name: "One Piece",
-        image: "https://images.seeklogo.com/logo-png/19/1/one-piece-logo-png_seeklogo-198712.png",
+        logo: "https://images.seeklogo.com/logo-png/19/1/one-piece-logo-png_seeklogo-198712.png",
+        background:
+            "https://market.dwarf-factory.com/ams-ecom/70783785-b0b6-4092-a90b-aa310e651f45/gallery-collections/684bdb4d5e48dc793ee861d9",
     },
     {
         name: "Attack on Titan",
-        image: "https://1000logos.net/wp-content/uploads/2025/01/Attack-on-Titan-Logo.jpg",
+        logo: "https://1000logos.net/wp-content/uploads/2025/01/Attack-on-Titan-Logo.jpg",
+        background:
+            "https://market.dwarf-factory.com/ams-ecom/70783785-b0b6-4092-a90b-aa310e651f45/gallery-collections/6865f0bcf8734a1154e5eef9",
     },
     {
         name: "Naruto",
-        image: "https://hipfonts.com/wp-content/uploads/2022/11/Naruto-Logo.jpg",
+        logo: "https://hipfonts.com/wp-content/uploads/2022/11/Naruto-Logo.jpg",
+        background:
+            "https://market.dwarf-factory.com/ams-ecom/70783785-b0b6-4092-a90b-aa310e651f45/gallery-collections/640acc5fe75d4778c1f37c5a",
     },
     {
         name: "Pokemon",
-        image: "https://images.seeklogo.com/logo-png/28/1/pokemon-logo-png_seeklogo-284734.png",
+        logo: "https://images.seeklogo.com/logo-png/28/1/pokemon-logo-png_seeklogo-284734.png",
+        background:
+            "https://market.dwarf-factory.com/ams-ecom/70783785-b0b6-4092-a90b-aa310e651f45/gallery-collections/640ad5fc9f8354145f9f2119",
     },
     {
         name: "Yu-Gi-Oh!",
-        image: "https://upload.wikimedia.org/wikipedia/commons/2/21/Yu-Gi-Oh%21.png",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/2/21/Yu-Gi-Oh%21.png",
+        background:
+            "https://market.dwarf-factory.com/ams-ecom/70783785-b0b6-4092-a90b-aa310e651f45/gallery-collections/684bda095e48dc793ee861bc",
     },
     {
         name: "G2 Esports",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKugIg0EWQJTK_DmBNCnif3QypizNVBouG1Q&s",
-    },
-    {
-        name: "The Lord of the Rings",
-        image: "https://images.seeklogo.com/logo-png/43/1/lord-of-the-rings-fellowship-of-the-ring-logo-png_seeklogo-431431.png",
-    },
-    {
-        name: "Fast Food",
-        image: "https://images.seeklogo.com/logo-png/27/1/fastfood-logo-png_seeklogo-274838.png",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKugIg0EWQJTK_DmBNCnif3QypizNVBouG1Q&s",
+        background:
+            "https://market.dwarf-factory.com/ams-ecom/70783785-b0b6-4092-a90b-aa310e651f45/gallery-collections/640ab34be75d4778c1f37b10",
     },
     {
         name: "Fnatic",
-        image: "https://market.dwarf-factory.com/ams-ecom/70783785-b0b6-4092-a90b-aa310e651f45/gallery-collections/641d75b7b707966653c42442",
+        logo: "https://market.dwarf-factory.com/ams-ecom/70783785-b0b6-4092-a90b-aa310e651f45/gallery-collections/641d75b7b707966653c42442",
+        background:
+            "https://market.dwarf-factory.com/ams-ecom/70783785-b0b6-4092-a90b-aa310e651f45/gallery-collections/640acd119f8354145f9f209e",
+    },
+    {
+        name: "Lord of The Rings",
+        logo: "https://images.seeklogo.com/logo-png/43/1/lord-of-the-rings-fellowship-of-the-ring-logo-png_seeklogo-431431.png",
+        background:
+            "https://market.dwarf-factory.com/ams-ecom/70783785-b0b6-4092-a90b-aa310e651f45/gallery-collections/6707fbff6df41770ae32b7ee",
+    },
+    {
+        name: "Fast Food",
+        logo: "https://images.seeklogo.com/logo-png/27/1/fastfood-logo-png_seeklogo-274838.png",
+        background:
+            "https://market.dwarf-factory.com/ams-ecom/70783785-b0b6-4092-a90b-aa310e651f45/gallery-collections/640aced17168d10d9ccba765",
     },
 ];
 
@@ -53,8 +71,14 @@ const seedCollectionsIfEmpty = async () => {
 
             if (created) {
                 inserted += 1;
-            } else if (collection.image !== col.image) {
-                await collection.update({ image: col.image });
+            } else if (
+                collection.logo !== col.logo ||
+                collection.background !== col.background
+            ) {
+                await collection.update({
+                    logo: col.logo,
+                    background: col.background,
+                });
             }
         }
 
