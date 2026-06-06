42a2d1e50f8422fd07064a819e9d265581a8c01d
TranAnhHuy <anhhuyrubic@gmail.com>
2026-06-06 22:09:03 +0700

    fix(models): add foreign key references for orderId and productId in OrderItem model

--- ExpressJS_BE/src/models/OrderItem.js
+++ ExpressJS_BE/src/models/OrderItem.js
@@ -13,11 +13,19 @@ const OrderItem = sequelize.define(
         orderId: {
             type: DataTypes.INTEGER.UNSIGNED,
             allowNull: false,
+            references: {
+                model: "orders",
+                key: "id",
+            },
         },
 
         productId: {
             type: DataTypes.INTEGER.UNSIGNED,
             allowNull: false,
+            references: {
+                model: "products",
+                key: "id",
+            },
         },
 
         sku: {
