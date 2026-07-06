2dd330ecae7472924af180dca8115ad783dde91b
TranAnhHuy <anhhuyrubic@gmail.com>
2026-07-06 23:32:39 +0700

    refactor(HomePage): adjust button width for better responsiveness

--- ReactJS_FE/src/pages/user/HomePage.jsx
+++ ReactJS_FE/src/pages/user/HomePage.jsx
@@ -568,7 +568,7 @@ const HomePage = () => {
                     <div className="flex flex-col items-center mt-8">
                         <button
                             onClick={() => navigate("/products")}
-                            className="h-12 w-40 bg-[var(--theme-accent)] px-5 py-2 items-center gap-2 font-oswald font-bold uppercase tracking-widest text-white hover:-translate-y-1 hover:shadow-[4px_4px_0_rgba(0,0,0,1)] hover:border-2 hover:border-black transition-all cursor-pointer"
+                            className="h-12 w-fit bg-[var(--theme-accent)] px-5 py-2 items-center gap-2 font-oswald font-bold uppercase tracking-widest text-white hover:-translate-y-1 hover:shadow-[4px_4px_0_rgba(0,0,0,1)] hover:border-2 hover:border-black transition-all cursor-pointer"
                         >
                             EXPLORE NOW
                         </button>
